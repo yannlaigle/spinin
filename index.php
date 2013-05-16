afa4600248de0dfdc70e3c915c2e76d7d4476034
yannlaigle <yann.laigle@gmail.com>
2013-05-16 20:30:50 +0200

    Envoie de l'index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="robot" content="all" />
+<meta name="keywords" content="id,accueil,identification,sign" />
+<meta name="title" content="Identification" />
+<title>Identification</title>
+    <link href="css/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="css/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+    <link href="css/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container" align="center">
+	<div class="row">
+    	<div class="span11"><br /><br /><br /><br />
+        	<h1>Bienvenue sur SpinIn</h1><br /><br />
+            <form class="form-inline" action="id/login.php" method="post">
+              <input type="email" name="login" class="input" placeholder="Email"><br /><br />
+              <input type="password" name="pwd" class="input" placeholder="Mot de passe"><br /><br /><br />
+              <button type="submit" class="btn btn-large btn-success">Sign in</button>
+            </form>
+		</div>
+    </div>
+</div>
+
+<script type="text/javascript" src="../css/js/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="../css/js/bootstrap.js"></script>
+
+</body>
+</html>
